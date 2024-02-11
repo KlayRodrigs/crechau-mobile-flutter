c3c30daf67025c9d089b7acb8a59dc2d289dda4d
klayrodrigs <KlayvertGab@protonmail.com>
2024-02-10 21:03:38 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/crechau_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crechau_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:crechau_mobile/app/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
